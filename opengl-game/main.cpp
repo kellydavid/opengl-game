b851f257537b226c4021b97ebc4e7ec4244e2283
kellydavid <kellydavid178@gmail.com>
2015-11-21 15:49:59 +0000

    Initial Commit

---
+++ opengl-game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl-game
+//
+//  Created by David Kelly on 21/11/2015.
+//  Copyright © 2015 David Kelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
