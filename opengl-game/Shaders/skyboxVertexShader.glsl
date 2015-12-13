43c6c743409bf9d6b71a3c2784cdb28aaaf7913f
kellydavid <kellydavid178@gmail.com>
2015-12-13 01:52:02 +0000

    Removed reflection from cube map shading so it renders much faster

--- opengl-game/Shaders/skyboxVertexShader.glsl
+++ opengl-game/Shaders/skyboxVertexShader.glsl
@@ -3,15 +3,14 @@
 layout (location = 0)in vec3 vertex_position;
 layout (location = 1)in vec3 vertex_normal;
 
-out vec3 LightIntensity;
-out vec3 position_eye, normal_eye;
+out vec3 texture_coordinates;
 
 uniform mat4 view;
 uniform mat4 proj;
 uniform mat4 model;
 
 void main(){
-    position_eye = vec3 (view * model * vec4 (vertex_position, 1.0));
-    normal_eye = vec3 (view * model * vec4 (vertex_normal, 0.0));
+    vec3 position_eye = vec3 (view * model * vec4 (vertex_position, 1.0));
+    texture_coordinates = vertex_position;
     gl_Position = proj * vec4 (position_eye, 1.0);
 }
