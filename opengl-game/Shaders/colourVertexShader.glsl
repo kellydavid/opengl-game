a02e0b087240e36b42326fc8fde4b22845089948
kellydavid <kellydavid178@gmail.com>
2015-12-17 19:32:15 +0000

    Fixed lighting for color shader

--- opengl-game/Shaders/colourVertexShader.glsl
+++ opengl-game/Shaders/colourVertexShader.glsl
@@ -4,12 +4,7 @@ layout (location = 0) in vec3 vertex_position;
 layout (location = 1) in vec3 vertex_normal;
 
 out vec3 LightIntensity;
-
-vec4 LightPosition = vec4 (10.0, 10.0, 4.0, 1.0); // Light position in world coords.
-//vec3 Kd = vec3 (0.0, 0.8, 0.0); // green diffuse surface reflectance
-vec3 Kd;
-vec3 Ld = vec3 (1.0, 1.0, 1.0); // Light source intensity
-
+out vec3 position_eye, normal_eye;
 
 uniform mat4 view;
 uniform mat4 proj;
@@ -19,22 +14,9 @@ uniform vec3 color;
 
 void main(){
     
-    mat4 ModelViewMatrix = view * model;
-    mat3 NormalMatrix =  mat3(ModelViewMatrix);
-    // Convert normal and position to eye coords
-    // Normal in view space
-    vec3 tnorm = normalize( NormalMatrix * vertex_normal);
-    // Position in view space
-    vec4 eyeCoords = ModelViewMatrix * vec4(vertex_position,1.0);
-    //normalised vector towards the light source
-    vec3 s = normalize(vec3(LightPosition - eyeCoords));
-    
-    // The diffuse shading equation, dot product gives us the cosine of angle between the vectors
-    Kd = color;
-    LightIntensity = Ld * Kd * max( dot( s, tnorm ), 0.0 );
-    
-    // Convert position to clip coordinates and pass along
-    gl_Position = proj * view * model * vec4(vertex_position,1.0);
+    position_eye = vec3 (view * model * vec4 (vertex_position, 1.0));
+    normal_eye = vec3 (view * model * vec4 (vertex_normal, 0.0));
+    gl_Position = proj * vec4 (position_eye, 1.0);
 }
 
 
